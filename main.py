d1b53d6ccfd90475731b89dfd3512952e197d4f6
Taofeeq Ayodele Feyibunmi <joingkayodele@gmail.com>
2025-05-05 04:55:47 +0100

    My  python tutorial

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hi Guys, I'm Learning python")
+# This is a Decision Statement
+if 10>5:
+    print("Ten is Greater Than Five!")
+    print("Ten is Greater Than Five!")
+    
+# Git Version Control
+# We are taking a step forward when it comes to our python learning
